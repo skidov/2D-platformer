381bc686bb09ca59005bc1e66be9b80e2fb7721b
Pongrácz Ádám <9999adam99@gmail.com>
2021-10-11 19:48:37 +0200

    Namespace-ek megváltoztatva

--- Platformer/Platformer/Objects/Characters/Character.cs
+++ Platformer/Platformer/Objects/Characters/Character.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Platformer.Objects.Characters
 {
     abstract class Character
     {
+        public Vector2 Position { get; set; }
     }
 }
--- Platformer/Platformer/Objects/SpriteSheet.cs
+++ Platformer/Platformer/Objects/SpriteSheet.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Platformer.Objects
 {
     class SpriteSheet
     {
+        private int widthCount;
+        private int heightCount;
+
     }
 }
