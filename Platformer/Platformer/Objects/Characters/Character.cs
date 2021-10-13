fb4cc08cebc8afaa57db97dfbd21e65882a99ed8
Pongrácz Ádám <9999adam99@gmail.com>
2021-10-13 22:10:41 +0200

    Update Character.cs

--- Platformer/Platformer/Objects/Characters/Character.cs
+++ Platformer/Platformer/Objects/Characters/Character.cs
@@ -7,5 +7,8 @@ namespace Platformer.Objects.Characters
     abstract class Character
     {
         public Vector2 Position { get; set; }
+
+        public abstract void Update(GameTime gameTime, SpriteBatch spriteBatch);
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
     }
 }
